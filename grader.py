6657036ac30e48e6c308b977c691b5b6ff4e3fb1
Victor Espinoza <tug86727@temple.edu>
2018-01-20 15:30:51 -0500

    cleaner, more dynamic?

--- grader.py
+++ grader.py
@@ -1,6 +1,5 @@
 import numpy as np
 import csv
-#import pandas as pd #print ("imported pandas!!")
 import tkinter as tk
 from tkinter import filedialog
 import os
@@ -8,119 +7,119 @@ import datetime
 
 getMore = True
 
-now = format(datetime.date.today())
-nows = str(now)
-now2 = format(datetime.datetime.now())
-
-
-print("please Select HacckerRank CSV file")
-HRfile = filedialog.askopenfilename(title = "Select HackerRank file",filetypes = (("CSV files","*.csv"),("all files","*.*")))  # get directory +filename.csv
-print(HRfile)
-head, HRtail = os.path.split(HRfile)
- 
-with open(HRfile) as csvDataFile:
-	csvReader = csv.reader(csvDataFile)
-	Hr = list(csv.reader(csvDataFile))
-
-numrows1 = len(Hr)    # 3 rows in your example
-numcols1 = len(Hr[0])
-print("rows by cols of Hr = ", numrows1,numcols1)
-
-print("please Select Canvas CSV file")
+#### Get a single Canvas CSV file to update! #############
+print("Please Select Canvas CSV file")
 Ca = filedialog.askopenfilename(title = "Select canvas file",filetypes = (("CSV files","*.csv"),("all files","*.*")))  # get directory +filename.csv
-	
-print(Ca)
- 
+head, tail = os.path.split(Ca)
+print("You are now editing canvas file: ",tail)
 with open(Ca) as csvDataFile:
 	csvReader = csv.reader(csvDataFile)
-	Can = list( csv.reader(csvDataFile) )
-		
-col = int(input("Enter the column to edit in Canvas: "))
-col = col-1
+	Can = list( csv.reader(csvDataFile))
+total_students = len(Can)-3
+print("for all ", total_students , " students! Hope this code works!")
+###############################################################
 
-points = int(input("Enter the how many points this assignment is worth in Canvas: "))
-scaleDown = points/100;
-numrows = len(Can)    # 3 rows in your example
-total_students = numrows -3
-numcols = len(Can[0])
-print("rows by cols of Canvas = ", numrows,numcols)
-print("you plan on editing canvas column ", col)
-
-email = []
-
-for rows in Hr:
-	thing = rows[2]
-
-for row in range(1, len(Hr)):
-	thing =Hr[row][2]
-	thing = thing.lower()
-	Hr[row].insert(17, thing)
+### this function gets the unique TU emai and inserts in the second column of the HR list
+def modHR(Hr):
+	email = []
+	for row in range(1, len(Hr)):
+		thing =Hr[row][2]
+		thing = thing.lower()
+		Hr[row].insert(17, thing)
 	
-	if "@" in thing:
-		a,b = thing.split('@')
-		email.append(a)
-		Hr[row].insert(18, a)
-		Hr[row][2] = a
+		if "@" in thing:
+			a,b = thing.split('@')
+			email.append(a)
+			Hr[row].insert(18, a)
+			Hr[row][2] = a
 
-	else:
-		email.append(thing)
-		print('Was not able to find @ symbol',thing)
-		Hr[row].insert(18, thing)
-		Hr[row][2] =thing
-		
-#newHR  = np.array(Hr)
-#newEmail = np.array(email)
-#newHR[1:,2] = newEmail
-matches = 0 
-nomatches = 0
-
-############# Time to Match TUid's!! 
-noMatchz = []
-for row in range(1, len(Hr)): # go thru each submission
-	tuID = Hr[row][2]
-	grade = float(Hr[row][11])
-	
-	for rowz in range(2, len(Can)-1):  # match with their respective canvas slot
-		canvasID = Can[rowz][2]
-		if canvasID == tuID:
-			Can[rowz][col] = grade*scaleDown
-			matches+=1
-			break
-		if rowz == numrows-2:
-			print("we could not match canvas id with submission associated with", tuID)
-			nomatches+=1
-			noMatchz.append(Hr[row][17])
-################
-	
-#for rowz in range(2, len(Can)-1):
-#	poo = Can[rowz][2]
-#	print(poo)
-
-print("Successfully matched submissions = ",matches)  #print how many submissions were matched
-print("Unmatched submissions", nomatches)		#print how many submissions were NOT matched
-
-
-##WRITE NEW CANVAS FILE  #one way to write canvas file
-#myfile = open('testCanvas.csv','w')
-#wr = csv.writer(myfile, lineterminator='\n')
-#wr.writerows(Can)
-#myfile.close()
-
-# WRITE NEW CANVAS FILE  #another way to write canvas file
-newCanName = now+"_newCanvasFile.csv"
-with open(newCanName, "w") as output:
-    writer = csv.writer(output, lineterminator='\n')
-    writer.writerows(Can)
+		else:
+			email.append(thing)
+			#print('Was not able to find @ symbol',thing)  # not always necessary unless you want to see all the types of emails students used to register for canvas
+			Hr[row].insert(18, thing)
+			Hr[row][2] =thing
+	return Hr
 	
 	
-with open('HR_mod.csv', "w") as output:
-    writer = csv.writer(output, lineterminator='\n')
-    writer.writerows(Hr)
+### this  function creates ann error log with a time stamp
+def writeErrorLog(nomatch):
+	now = format(datetime.date.today())
+	nows = str(now)
+	noMatchName = nows +'_unMatchedReport_' + HRtail
+	with open(noMatchName, "w") as output:
+		writer = csv.writer(output, lineterminator='\n')
+		for val in nomatch:
+			writer.writerow([val]) 
+	print("Error Log file created for " ,HRtail,"!")
+	print("Saved as ", noMatchName)
+	print("Exported report with submissions that were not matched to a student in canvas")
+
+### this  function creates a new updated canvas file with a time stamp
+def newCanvas(nc):
+	now = format(datetime.date.today())
+	nows = str(now)
+	newCanName = nows+"_UpdatedCanvasFile.csv"
+	with open(newCanName, "w") as output:
+		writer = csv.writer(output, lineterminator='\n')
+		writer.writerows(nc)
+	print("Successfully exported ",newCanName)
+	print("and it is ready to be uploaded to canvas!")
+
 	
-		
-#Assuming noMatchz is a flat list write 
-noMatchName = nows +'_unMatchedReport_' + HRtail
-with open(noMatchName, "w") as output:
-    writer = csv.writer(output, lineterminator='\n')
-    for val in noMatchz:
-        writer.writerow([val])  
+### this function updates a column in the canvas file 
+def updateCanvas(ca, hr, col, scale):
+	nomatches = 0
+	matches = 0 
+	noMatchz = []
+	for row in range(1, len(hr)): # go thru each submission in hackerRank
+		tuID = hr[row][2]
+		grade = float(hr[row][11])
+	
+		for rowz in range(2, len(ca)-1):  # match with their respective canvas slot
+			canvasID = ca[rowz][2]
+			if canvasID == tuID:
+				ca[rowz][col] = grade*scale
+				matches+=1
+				break
+			if rowz == len(ca)-2:
+				print("we could not match canvas id with submission associated with", tuID)
+				nomatches+=1
+				noMatchz.append(hr[row][17])
+	writeErrorLog(noMatchz)
+	
+	return ca
+	
+
+
+while(getMore):
+	#### lets get a HR file and put it in a list
+	print("Please Select HackerRank CSV file")  ####### 
+	HRfile = filedialog.askopenfilename(title = "Select HackerRank file",filetypes = (("CSV files","*.csv"),("all files","*.*")))  # get directory +filename.csv
+	head, HRtail = os.path.split(HRfile)
+	with open(HRfile) as csvDataFile:
+		csvReader = csv.reader(csvDataFile)
+		Hr = list(csv.reader(csvDataFile))
+	print("You have successfully imported HR file ", HRtail)	
+	numrows1 = len(Hr)          # 3 rows in your example
+	numcols1 = len(Hr[0])
+	print("Refer to CanvasColumn.xlsx file in TA drive")
+	col = int(input("Enter the column to edit in CANVAS: "))
+	col = col+3  ######## i think, double check the google doc 
+	#### Get user to define how many points this assignment is worth #############	
+	points = int(input("Enter how many points this assignment is worth in Canvas: "))
+	scaleDown = points/100
+	
+	newHR = modHR(Hr)           # edit the HackeRank file first ..... 
+	newCa = updateCanvas(Can,newHR,col,scaleDown)  #now update the canvas File!
+	
+	decide = int(input("Do you wish to continue updating? yes=1, no=0 "))
+
+	if decide == 1:
+		getMore = True
+	if decide == 0:
+		getMore = False
+		newCanvas(newCa)
+		print("Thank you for using grader.py! \nTo provide feedback or report bugs, email Victor at tug86727@temple.edu")
+
+	
+	
